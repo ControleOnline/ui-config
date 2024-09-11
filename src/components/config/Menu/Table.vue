8718f00a0ee6f8e54a30e0d164cf72273dfa8a10
BrunoGaffo <67167417+BrunoGaffo@users.noreply.github.com>
2024-09-10 21:53:07 -0300

    up

--- src/components/config/Menu/Table.vue
+++ src/components/config/Menu/Table.vue
@@ -3,19 +3,38 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
+
 export default {
   components: {},
 
   computed: {
+    ...mapGetters({
+      defaultCompany: "people/defaultCompany",
+    }), 
     configs() {
       return {
         externalFilters: false,
-        store: "menus",        add: true,
+        store: "menus",
+        categories: ["menu"],
+        add: true,
         delete: true,
         selection: false,
         search: false,
+        columns: {
+          category: {
+            filters: {
+              context: "menu",
+              company: "/people/" + this.defaultCompany.id,
+            },
+          },
+        },
       };
     },
+     
   },
 };
 </script>
+
+
