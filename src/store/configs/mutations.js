cd81bcb50b3f61331b1d9d21c3f21ebc0a90fe7b
Luiz Kim <luiz.kim@controleonline.com>
2025-02-07 14:09:42 -0300

    Redux

--- src/store/configs/mutations.js
+++ src/store/configs/mutations.js
@@ -1,10 +1,8 @@
-import { LocalStorage } from 'quasar';
-import * as types from './mutation_types';
+import * as types from "./mutation_types";
 
 export default {
-
-  [types.SET_MODULE  ](state, module) {
-    Object.assign(state, { module });
+  [types.SET_MODULE](state, payload) {
+    if (!payload?.module) Object.assign(state, { module: payload });
+    return { ...state, module: payload?.module || payload };
   },
-
 };
