33becf46eff2a0c58d3ac6e2d5908b2dad631294
Luiz Kim <luizkim@users.noreply.github.com>
2026-08-01 16:04:55 -0300

    feat: migrate tenancies list to DefaultTable

--- src/store/tenancies/index.js
+++ src/store/tenancies/index.js
@@ -1,16 +1,133 @@
-import * as actions from './actions';
-import * as getters from './getters';
-import mutations from './mutations';
+import * as actions from '@controleonline/ui-default/src/store/default/actions';
+import * as getters from '@controleonline/ui-default/src/store/default/getters';
+import defaultMutations from '@controleonline/ui-default/src/store/default/mutations';
+import * as customActions from './actions';
+import customMutations from './mutations';
 
 export default {
   namespaced: true,
   state: {
+    item: {},
     items: [],
+    resourceEndpoint: 'tenancies',
     isLoading: false,
     isSaving: false,
     error: '',
+    totalItems: 0,
+    summary: {},
+    filters: {},
+    messages: [],
+    message: {},
+    columns: [
+      {
+        editable: false,
+        isIdentity: true,
+        sortable: true,
+        name: 'id',
+        align: 'left',
+        label: 'id',
+        format(value) {
+          return `#${value}`;
+        },
+      },
+      {
+        sortable: true,
+        name: 'appHost',
+        align: 'left',
+        label: 'appHost',
+        format(value) {
+          return value;
+        },
+      },
+      {
+        sortable: true,
+        name: 'dbHost',
+        align: 'left',
+        label: 'dbHost',
+        format(value) {
+          return value;
+        },
+      },
+      {
+        sortable: true,
+        name: 'dbName',
+        align: 'left',
+        label: 'dbName',
+        format(value) {
+          return value;
+        },
+      },
+      {
+        sortable: true,
+        name: 'dbPort',
+        align: 'left',
+        label: 'dbPort',
+        inputType: 'number',
+        format(value) {
+          return value;
+        },
+      },
+      {
+        sortable: true,
+        name: 'dbUser',
+        align: 'left',
+        label: 'dbUser',
+        format(value) {
+          return value;
+        },
+      },
+      {
+        sortable: true,
+        name: 'dbDriver',
+        align: 'left',
+        label: 'dbDriver',
+        format(value) {
+          return value;
+        },
+      },
+      {
+        sortable: true,
+        name: 'dbInstance',
+        align: 'left',
+        label: 'dbInstance',
+        format(value) {
+          return value;
+        },
+      },
+      {
+        sortable: true,
+        name: 'installationStatus',
+        align: 'left',
+        label: 'installationStatus',
+        list: [
+          { value: 'pending', label: 'pending' },
+          { value: 'installed', label: 'installed' },
+          { value: 'failed', label: 'failed' },
+        ],
+        format(value) {
+          return value;
+        },
+      },
+      {
+        editable: true,
+        name: 'dbPassword',
+        align: 'left',
+        label: 'dbPassword',
+        inputType: 'password',
+        table: false,
+        format() {
+          return '';
+        },
+      },
+    ],
   },
-  actions,
+  actions: {
+    ...actions,
+    ...customActions,
+  },
   getters,
-  mutations,
+  mutations: {
+    ...defaultMutations,
+    ...customMutations,
+  },
 };
